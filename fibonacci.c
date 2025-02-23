08d472fb41c9808ab09f65dee85421731858cc54
DylGit3 <senddylmail27@gmail.com>
2025-02-22 17:49:01 -0800

    updated fibonacci

--- fibonacci.c
+++ fibonacci.c
@@ -1,42 +1,44 @@
-// Copied the code from the slides. Also experiemnted using unsigned long long
-// to expand how many values of fibonacci could be reached
-
 #include <stdint.h>
 #include <stdio.h>
 
-typedef unsigned long long (*ull_func_ptr)(long number);
+typedef unsigned long long (*ull_func_ptr)(int number);
 
 #define NOT_PRESENT (unsigned long long)(-1)
-#define MAX_N 100
+#define MAX_N 93
 
 static unsigned long long results[MAX_N];
 ull_func_ptr fibonacci_provider;
 ull_func_ptr original_provider;
 
-unsigned long long fibonacci(long number) {
+unsigned long long fibonacci(int number) {
     if (number < 0) {
         fprintf(stderr, "Value must be greater than or equal to 0.\n");
         return NOT_PRESENT;
     }
+
     return original_provider(number);
 }
 
-unsigned long long fibonacci_calculations(long number) {
+unsigned long long fibonacci_calculations(int number) {
     if (number == 0)
         return 0;
     if (number == 1)
         return 1;
+    if (number >= MAX_N)
+        return original_provider(number - 1) + original_provider(number - 2);
+
     return fibonacci_provider(number - 1) + fibonacci_provider(number - 2);
 }
 
-unsigned long long cache(long value) {
+unsigned long long cache(int value) {
     if (results[value] == NOT_PRESENT)
         results[value] = original_provider(value);
+
     return results[value];
 }
 
 int main() {
-    long n = 97;
+    int n = 97;
 
     for (int i = 0; i < MAX_N; i++)
         results[i] = NOT_PRESENT;
@@ -44,7 +46,7 @@ int main() {
     original_provider  = fibonacci_calculations;
     fibonacci_provider = cache;
 
-    printf("The value of %ld is %llu.\n", n, fibonacci(n));
+    printf("The value of %d is %llu.\n", n, fibonacci(n));
 
     return 0;
 }
